2d7dc381cc153c99b29ec629d271991cadec36b7
marcin <marcin.migdal@fortum.com>
2026-03-17 15:28:12 +0100

    setup react js project

--- marcin-react/src/App.test.js
+++ marcin-react/src/App.test.js
@@ -1,10 +1,20 @@
-
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
+import {suma} from './sum/sum'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App/>);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
+
+describe('suma function w APP test', () => {
+    it('should return proper sum w APP test', () => {
+        const result = suma(1, 2);
+        expect(result).toBe(3)
+    });
+});
+
+
+
 
